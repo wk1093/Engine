beeada4f1f2b5be139ae71b3ef7293ab560ea54c
wk1093 <wyattk1093@gmail.com>
2023-02-22 14:46:31 -0800

    YOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO, COLORED RECTANGLE

--- engine/API/Mesh.h
+++ engine/API/Mesh.h
@@ -1,3 +1,11 @@
 #pragma once
+#include "apiHeader.h"
 
+#include "Vertex.h"
 
+namespace Engine {
+    class Mesh {
+        std::vector<Vertex> vertices;
+        std::vector<unsigned int> indices;
+    };
+}
--- engine/API/Renderer.h
+++ engine/API/Renderer.h
@@ -15,7 +15,7 @@ namespace Engine {
         Renderer(); // test constructor TODO: Remove this.
         Renderer(const std::string& pathToObj, const Color& color, const std::string& texPath="");
         Renderer(const std::vector<Vertex> vertices, const std::string& texPath="");
-        //Renderer(const Mesh&, const std::string& texPath="");
+        Renderer(const Mesh&, const std::string& texPath="");
 
         void render();
 
